07428c3b4c4840f62a49b3349564d949a40a907e
“” <minty@bu.edu>
2024-02-22 10:01:42 -0500

    run batch by batch

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -l
 
-#run the command in terminal: bash ./start.sh sample_list.txt path_to_reads_folder
+#run the command in terminal: bash ./start.sh list.txt /restricted/projectnb/casa/mtLin/bu_brain_rna/batch4/minus/rawdata
 nsamples=$(wc -l < ${1})
 echo "The number of samples is ${nsamples}"
+echo "The path: ${2}"
 qsub -t 1-${nsamples} runCIR3.qsub ${1} ${2}
